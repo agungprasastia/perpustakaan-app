4a0542abe9b39edf2495769557fb2dc17bf22670
Agung Prasasti Abadi <agungprasastia@gmail.com>
2025-11-15 21:10:56 +0800

    feat(buku): Implement Buku CRUD functionality

--- modules/buku/buku_hapus.php
+++ modules/buku/buku_hapus.php
@@ -1,15 +1,27 @@
 <?php
-include '../../config/koneksi.php';
+include '../../config/koneksi.php'; 
 
 if (isset($_GET['id'])) {
+    
     $id_buku = (int)$_GET['id'];
-    $sql = "DELETE FROM buku WHERE id_buku = $id_buku";
+    
+    $sql_del_peminjaman = "DELETE FROM peminjaman WHERE id_buku = $id_buku";
+    $hasil_peminjaman = mysqli_query($koneksi, $sql_del_peminjaman);
 
-    if (mysqli_query($koneksi, $sql)) {
-        header("Location: ../../index.php?status=sukses_hapus");
-        exit;
+    if ($hasil_peminjaman) {
+        
+        $sql_del_buku = "DELETE FROM buku WHERE id_buku = $id_buku";
+        $hasil_buku = mysqli_query($koneksi, $sql_del_buku);
+        
+        if ($hasil_buku) {
+            header("Location: ../../index.php?status=sukses_hapus_buku");
+            exit;
+        } else {
+            echo "Error: Gagal menghapus buku. " . mysqli_error($koneksi);
+        }
+        
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($koneksi);
+        echo "Error: Gagal menghapus riwayat peminjaman terkait. " . mysqli_error($koneksi);
     }
     
 } else {
